45aa28a2c956f36089a439685c9793c95b755838
Anthony Benites <anthony.benites17@gmail.com>
2017-09-25 07:45:20 -0700

    add full upload.csv functionality

--- dailysales.py
+++ dailysales.py
@@ -7,9 +7,19 @@ from openpyxl import load_workbook
 import os
 import sys
 
-def create_sales_df(now):
+# Date Variables
+def ds_start():
+    # List all spreadsheets that aren't the main DS workbook
+    file = [each for each in os.listdir(cwd) if each.endswith('-2017.xlsx') and 'Daily Sales' not in each][0]
+    if len(files) != 1:
+        error_file('More than one file found')
+        sys.exit()
+    else:
+        create_sales_df(filename)
+
+def create_sales_df(file):
     # Import Sales
-    sales_date = now['month'] + '-' + now['year']
+    sales_date = filename
     sales_xl = pd.ExcelFile(sales_date + '.xlsx')
     sales_df = sales_xl.parse('Sheet1')
     sales_df = sales_df[['GL Account #', 'Site', 'End Date', '(Item) Name', 'Amount']]
@@ -26,6 +36,7 @@ def create_sales_df(now):
 
     # Match Description to GL Codes (VLOOKUP)
     sales_df['GL Account #'] = sales_df['(Item) Name'].map(gl_df_base['GL Account #'].astype(str))
+    sales_df['PWC'] = sales_df['(Item) Name'].map(gl_df_base['PWC'].astype(str))
     sales_df['Site_ID'] = sales_df['Site'].map(gl_df_site['Site_ID'].astype(str))
     
     # Items that aren't GL coded - add to dailysales
@@ -67,14 +78,30 @@ def create_sales_df(now):
         sales_df_date = sales_df[sales_df['End Date']==date]
         
         # Enter new data for 2164 account
+        cols = ['GL Account #', 'Site', 'End Date', '(Item) Name', 'Amount', 'Site_ID']
         sales_df_date_2164 = sales_df_date[sales_df_date['GL Account #']=='2164']
         if not sales_df_date_2164.empty and round(sales_df_date_2164['Amount'].sum(),2) != 0:
             new_2164 = round(sales_df_date_2164['Amount'].sum(),2)
             new_2165 = round(sales_df_date_2164['Amount'].sum(),2) * -1
             
-            df_2164 = pd.DataFrame([['2164', site_name, date, 'GSR B1G1 Sold', new_2164, site_id]],columns=['GL Account #', 'Site', 'End Date', '(Item) Name', 'Amount', 'Site_ID'])
-            df_2165 = pd.DataFrame([['2165', site_name, date, 'GSR B1G1 Sold Dscnt', new_2165, site_id]],columns=['GL Account #', 'Site', 'End Date', '(Item) Name', 'Amount', 'Site_ID'])
+            df_2164 = pd.DataFrame([['2164', site_name, date, 'GSR B1G1 Sold', new_2164, site_id]],columns=cols)
+            df_2165 = pd.DataFrame([['2165', site_name, date, 'GSR B1G1 Sold Dscnt', new_2165, site_id]],columns=cols)
             df_entries = df_2164.append(df_2165, ignore_index=True)
+            sales_df = sales_df.append(df_entries, ignore_index=True)
+        
+        # PWC
+        sales_df_date_PWC = sales_df_date[sales_df_date['PWC']=='x']
+        if not sales_df_date_PWC.empty and round(sales_df_date_PWC['Amount'].sum(),2) != 0:
+            new_1201 = round(sales_df_date_PWC['Amount'].sum() * 0.2,2)
+            new_2163 = round(sales_df_date_PWC['Amount'].sum() * 0.8,2)
+            new_2162 = (new_1201 + new_2163) * -1
+
+            df_1201 = pd.DataFrame([['1201', site_name, date, 'Accounts Receivable', new_1201, site_id]],columns=cols)
+            df_2163 = pd.DataFrame([['2163', site_name, date, 'GSR PC - Discount', new_2163, site_id]],columns=cols)
+            df_2162 = pd.DataFrame([['2162', site_name, date, 'GSR PC - Dscnt', new_2162, site_id]],columns=cols)
+            df_entries = df_1201.append(df_2163, ignore_index=True)
+            df_entries = df_entries.append(df_2162, ignore_index=True)
+            
             sales_df = sales_df.append(df_entries, ignore_index=True)
         
         # Does this day reconcile?
@@ -98,6 +125,12 @@ def create_sales_df(now):
     update_workbook(now, sales_df)
 
 def update_workbook(now, sales_df):
+    # Date Setup
+    month_names = {'1': 'January', '2': 'February', '3': 'March',
+            '4': 'April', '5': 'May', '6': 'June',
+            '7': 'July', '8': 'August', '9': 'September',
+            '10': 'October', '11': 'November', '12': 'December'}
+    
     # Index Site Name
     sites = sales_df.Site.unique().tolist()
     if len(sites) == 1:
@@ -106,7 +139,9 @@ def update_workbook(now, sales_df):
         error_file('Number of Sites Error: ' + str(sites))
 
     # Account Summary
-    sales_df_sum = sales_df.groupby('ACCOUNT')['Amount'].sum()
+    sales_df_sum = sales_df[sales_df['GL Account #'] != '1099'].groupby('ACCOUNT')['Amount'].sum()
+    sales_df_1099 = sales_df[sales_df['GL Account #'] == '1099'].groupby('(Item) Name')['Amount'].sum()
+    sales_df_sum = sales_df_sum.append(sales_df_1099)
 
     # Slice GL Upload Format
     upload_format = ['RECORD', 'ACCOUNT', 'ACCNTG DATE', 'JOURNAL', 'REF 1', 'REF 2', 'DESCRIPTION', 'DEBIT', 'CREDIT', 'ACCRUAL OR CASH']
@@ -146,7 +181,7 @@ def update_workbook(now, sales_df):
     sales_df_sum.reset_index().to_excel(writer, 'Summary', header=True, index=False)
     writer.save()
     
-    # create_upload_file(ds_filename)
+    create_upload_file(ds_filename)
 
 def create_upload_file(filename):
     # Load workbook
@@ -158,7 +193,7 @@ def create_upload_file(filename):
         if ws != 'Summary':
             upload_df = upload_df.append(xls.parse(ws), ignore_index=True)
     # Save DF as CSV
-    upload_df.to_csv('upload_file.csv', header=True, index=False)
+    upload_df.to_csv('{}.csv'.format(filename), header=True, index=False)
 
 
 def error_file(error):
@@ -169,31 +204,6 @@ def error_file(error):
         file.close()
     except:
         print('Could not create error file: ' + error)
-
-# Date Variables
-def ds_start():
-    # List all spreadsheets that aren't the main DS workbook
-    files = [each for each in os.listdir(cwd) if each.endswith('-2017.xlsx') and 'Daily Sales' not in each and 'dailysales' not in each]
-    if len(files) != 1:
-        error_file('More than one file found')
-        sys.exit()
-    
-    # Date Setup
-    month_names = {'1': 'January', '2': 'February', '3': 'March',
-                    '4': 'April', '5': 'May', '6': 'June',
-                    '7': 'July', '8': 'August', '9': 'September',
-                    '10': 'October', '11': 'November', '12': 'December'}
-
-    dates = []
-    for file in files:
-        file = file.replace('.xlsx', '').split('-')
-        file_date = {'month-name': month_names[str(file[0])],
-                'month': file[0],
-                'year': file[1]}
-        dates.append(file_date)
-
-    for now in dates:
-        create_sales_df(now)
 
 # Main
 if __name__ == '__main__':
